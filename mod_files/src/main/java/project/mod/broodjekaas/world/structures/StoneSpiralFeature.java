92cbc3007f60743a29117be6664dd2fc3a06489a
xGodScent <71233972+xGodScent@users.noreply.github.com>
2021-02-22 18:23:38 +0100

    tree gen

--- mod_files/src/main/java/project/mod/broodjekaas/world/structures/StoneSpiralFeature.java
+++ mod_files/src/main/java/project/mod/broodjekaas/world/structures/StoneSpiralFeature.java
@@ -26,7 +26,7 @@ public class StoneSpiralFeature extends Feature<DefaultFeatureConfig> {
     // stone spiral is defined to call for register
     public static final Feature<DefaultFeatureConfig> STONE_SPIRAL = new StoneSpiralFeature(DefaultFeatureConfig.CODEC);
 
-    //
+    // penis
     public static final ConfiguredFeature<?, ?> STONE_SPIRAL_CONFIGURED = STONE_SPIRAL.configure(FeatureConfig.DEFAULT)
     .decorate(Decorator.CHANCE.configure(new ChanceDecoratorConfig(5)));
 
@@ -35,49 +35,70 @@ public class StoneSpiralFeature extends Feature<DefaultFeatureConfig> {
     @Override
     public boolean generate(StructureWorldAccess world, ChunkGenerator generator, Random random, BlockPos pos, DefaultFeatureConfig config) {
       
-      
+      // some shit we need
       BlockPos topPos = world.getTopPosition(Heightmap.Type.WORLD_SURFACE, pos);
       Direction offset = Direction.NORTH;
 
 
-      for (int logY = 0; logY < random.nextInt(3)+4; logY++) {
-        
+      // generates tree
+      boolean done = false;
+      for (int logY = (random.nextInt(4)+4); logY > 0; logY--) {
         offset = offset.getOpposite();
+
+        // generates logs -> each iteration it adds a block downwards
         world.setBlockState(topPos.up(logY), ModBlocks.CHEESE_BLOCK.getDefaultState(), 3);
 
+
+        // generates leaves -> makes sure we only get leaves on the top blocks (and not all the way down)
+        if (!done) {
+          for (int x = 0; x < random.nextInt(3)+3; x++) { // generates base leaves -> cross shape
+
+            offset = offset.getOpposite();
+            world.setBlockState(topPos.up(logY+1).north(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+            world.setBlockState(topPos.up(logY+1).east(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+            world.setBlockState(topPos.up(logY+1).south(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+            world.setBlockState(topPos.up(logY+1).west(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+
+            // we have to 
+            for (int xz = 1; xz < x+1; xz++) {
+              
+              // draws a square into a certain dimension
+              world.setBlockState(topPos.up(logY+1).north(x).east(xz), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+              world.setBlockState(topPos.up(logY+1).north(xz).east(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+              world.setBlockState(topPos.up(logY+1).north(x).east(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+              world.setBlockState(topPos.up(logY+1).north(xz).east(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+
+              // repeat it for other dimensions
+              world.setBlockState(topPos.up(logY+1).east(x).south(xz), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+              world.setBlockState(topPos.up(logY+1).east(xz).south(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+              world.setBlockState(topPos.up(logY+1).east(x).south(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+              world.setBlockState(topPos.up(logY+1).east(xz).south(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+
+              world.setBlockState(topPos.up(logY+1).south(x).west(xz), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+              world.setBlockState(topPos.up(logY+1).south(xz).west(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+              world.setBlockState(topPos.up(logY+1).south(x).west(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+              world.setBlockState(topPos.up(logY+1).south(xz).west(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+
+              world.setBlockState(topPos.up(logY+1).west(x).north(xz), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+              world.setBlockState(topPos.up(logY+1).west(xz).north(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+              world.setBlockState(topPos.up(logY+1).west(x).north(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+              world.setBlockState(topPos.up(logY+1).west(xz).north(x), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3);
+
+
+            }
+            
+
+
+            // add for extra block in the middle so leaves grow around instead of on top
+            world.setBlockState(topPos.up(logY+1), ModBlocks.CHEESE_BLOCK.getDefaultState(), 3);
+        
+          }
+        }
+
+        done = true;
+
       }
-
-      // works!
-      world.setBlockState(world.getTopPosition(Heightmap.Type.WORLD_SURFACE, pos).down(1)
-        .offset(Direction.NORTH, 4), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 1);
-      
-      // doesnt fucking work??????? WHAT WHY???!!!! I ONLY PUT IT IN A LOOP WTF IS THIS BULLSHIT
-      for (int lDistX = 4; lDistX == 1; lDistX--) {
-
-        // fr who tf came up with this shit IM SO FUCKING DONE
-        
-        world.setBlockState(world.getTopPosition(Heightmap.Type.WORLD_SURFACE, pos).down(1)
-        .offset(Direction.NORTH, lDistX), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 1); // kanker
-        
-        world.setBlockState(world.getTopPosition(Heightmap.Type.WORLD_SURFACE, pos).down(1)
-        .offset(Direction.EAST, lDistX), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 1);
-        
-        world.setBlockState(world.getTopPosition(Heightmap.Type.WORLD_SURFACE, pos).down(1)
-        .offset(Direction.SOUTH, lDistX), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 1);
-       
-        world.setBlockState(world.getTopPosition(Heightmap.Type.WORLD_SURFACE, pos).down(1)
-        .offset(Direction.WEST, lDistX), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 1);
-
-
-      }
-
-      
-
-      // world.setBlockState(world.getTopPosition(Heightmap.Type.WORLD_SURFACE, pos).offset(Direction.random(random), random.nextInt(3)), ModBlocks.ORE_SUPER_CHEESE.getDefaultState(), 3, 14);
-      
-      
-      
-      //new BlockPos(3, 3, 3)
+    
 
 
       return true;
